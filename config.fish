517239f506d1c07686246cef8347b216d62f2057
Yuki <nekoy@debian.home>
2023-11-15 13:44:49 +0000

    added some aliases

--- config.fish
+++ config.fish
@@ -1,10 +1,16 @@
 if status is-interactive
     set fish_greeting                                 # Supresses fish's intro message
-    set nv "nvim"
-     if status is-interactive
-     and not set -q TMUX
-          exec tmux
-     end
+         if status is-interactive
+         and not set -q TMUX
+              exec tmux
+         end
+         #    exec neofetch
     #theme
     #fish_config theme save "Catppuccin Mocha"
+    alias la="lsd -hA --group-dirs first"
+    alias l="lsd"
+    alias ll="lsd -hlA --group-dirs first"
+    alias nv="nvim"
+    alias cr="clear"
+    alias lg="lazygit"
 end
