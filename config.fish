5f40e7d7a43b77f69781919583dd9f7f43435de1
Yuki <nekoy@debian.home>
2023-11-15 14:15:23 +0000

    added some comments

--- config.fish
+++ config.fish
@@ -1,16 +1,21 @@
 if status is-interactive
-    set fish_greeting                                 # Supresses fish's intro message
-         if status is-interactive
-         and not set -q TMUX
-              exec tmux
-         end
-         #    exec neofetch
-    #theme
-    #fish_config theme save "Catppuccin Mocha"
+    # Supresses fish's intro message
+    set fish_greeting
+
+    # automaticaly start tmux
+    if status is-interactive
+    and not set -q TMUX
+        exec tmux
+    end
+#   #theme
+#   fish_config theme save "Catppuccin Mocha"
+
+    # aliases
     alias la="lsd -hA --group-dirs first"
     alias l="lsd"
     alias ll="lsd -hlA --group-dirs first"
     alias nv="nvim"
     alias cr="clear"
     alias lg="lazygit"
+
 end
